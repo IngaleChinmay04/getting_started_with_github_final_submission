532a59821fc409acda51a837255fa4c3f87864bf
Chinmay Ingale <143017442+IngaleChinmay04@users.noreply.github.com>
2025-04-14 13:32:21 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Simple Interest Calculator
+# Formula: (P x R x T)/100
+
+echo "Enter Principal:"
+read P
+echo "Enter Rate of Interest:"
+read R
+echo "Enter Time (in years):"
+read T
+
+SI=$(echo "scale=2; ($P * $R * $T) / 100" | bc)
+echo "Simple Interest is: $SI"
